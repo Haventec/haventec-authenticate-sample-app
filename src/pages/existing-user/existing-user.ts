1adcf76036fa9d3e3e8ce316c0c2f365ec4a79ba
John Kelleher <john.kelleher@haventec.com>
2017-08-12 10:33:56 +1000

    Adds the existingi user page

--- src/pages/existing-user/existing-user.ts
+++ src/pages/existing-user/existing-user.ts
@@ -1,24 +1,61 @@
 import { Component } from '@angular/core';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 import { NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the ExistingUserPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
+import { HaventecClient } from '@haventec/common-js';
+import { AuthService } from '../../providers/auth-service/auth-service';
+import { LogService } from '../../providers/log-service/log-service';
+import { PageLoadingService } from '../../providers/page-loading-service/page-loading-service';
 
 @Component({
   selector: 'page-existing-user',
   templateUrl: 'existing-user.html',
+  providers: [AuthService]
 })
 export class ExistingUserPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  private addDeviceFormGroup : FormGroup;
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private formBuilder: FormBuilder,
+    private haventecClient: HaventecClient,
+    private authService: AuthService,
+    private logService: LogService,
+    private pageLoadingService: PageLoadingService
+  ) {
+    const self: any = this;
+
+    self.addDeviceFormGroup = self.formBuilder.group({
+      username: ['', Validators.required],
+      deviceName: ['', Validators.required]
+    });
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ExistingUserPage');
-  }
+  addDevice(){
+    const self: any = this;
 
+    let username = self.addDeviceFormGroup.value.username;
+    let deviceName = self.addDeviceFormGroup.value.deviceName;
+
+    self.logService.trace('Add device Username ' + username);
+    self.logService.trace('Add device name ' + deviceName);
+
+    self.pageLoadingService.present();
+
+    self.authService.addDevice(username, deviceName).then(
+      data => {
+        self.pageLoadingService.dismiss();
+        self.logService.trace('Add device response data ' + data);
+
+        self.haventecClient.init(username);
+        //self.navCtrl.push(ActivateAccountPage);
+      },
+      err => {
+        self.pageLoadingService.dismiss();
+        self.logService.error(err);
+      }
+    );
+
+  }
 }
--- src/providers/auth-service/auth-service.ts
+++ src/providers/auth-service/auth-service.ts
@@ -9,6 +9,7 @@ export class AuthService {
   private readonly url: string = Constant.API_ENDPOINT;
   private signUpUserPath: string = '/self-service/user';
   private registerUserPath: string = '/register/user';
+  private addDevicePath: string = '/device';
   private loginUserPath: string = '/login';
   // private logoutUserPath: string = '/logout';
   private forgotPinPath: string = '/forgot-pin';
@@ -45,6 +46,18 @@ export class AuthService {
     this.logService.trace('Activate account request data ' + body);
 
     return this.haventecClient.http.postNoAuth(this.url + this.registerUserPath, body);
+  }
+
+  addDevice(username: string, deviceName: string) {
+    let body = {
+      applicationUuid: Constant.APPLICATION_UUID,
+      username: username,
+      devicename: deviceName
+    };
+
+    this.logService.trace('Add device request data ' + body);
+
+    return this.haventecClient.http.postNoAuth(this.url + this.addDevicePath, body);
   }
 
   login(username: string, deviceUuid: string, authKey: string, hashedPin: string) {
