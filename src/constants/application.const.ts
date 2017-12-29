7658e277b950cb3030ab1e8825cd0fb44d9c59f8
John Kelleher <john.kelleher@haventec.com>
2017-12-29 12:17:35 +1100

    Removes Cloud Portal from comment

--- src/constants/application.const.ts
+++ src/constants/application.const.ts
@@ -1,7 +1,7 @@
 // Your backend server that communicates with Haventec. Sample backend server available at https://github.com/Haventec/haventec-authenticate-sample-server
 export const API_ENDPOINT: string = '';
 
-// Your Application UUID you received from Haventec Cloud Portal (cloudportal.haventec.com)
+// Your Application UUID you received from Haventec Console (cloudportal.haventec.com)
 export const APPLICATION_UUID: string = '';
 export const APPLICATION_NAME: string = 'Haventec';
 
