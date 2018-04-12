ff7775ff03751f1d53935723e8c24c33b99c1a4e
John Kelleher <john.kelleher@haventec.com>
2018-04-12 14:41:26 +1000

    Removes logs to clean up class

--- src/providers/http-service/http-service.ts
+++ src/providers/http-service/http-service.ts
@@ -1,26 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HaventecClient } from '@haventec/common-js';
-import { LogService } from '../../providers/log-service/log-service';
 
 @Injectable()
 export class HttpService {
 
-  constructor(private haventecClient: HaventecClient,
-              private logService: LogService) {
+  constructor(private haventecClient: HaventecClient) {
   }
 
   get(url: string): Promise<any> {
 
-    this.logService.trace('GET Request: ' + url);
-
     return new Promise((resolve, reject) => {
       this.haventecClient.http.get(url, this.haventecClient.getAccessToken()).then(
         data => {
-          this.logService.trace('GET Response: ' + url + ' with data ', data);
           resolve(data);
         },
         err => {
-          this.logService.error(err);
           reject(err);
         }
       );
@@ -29,16 +23,10 @@ export class HttpService {
 
   post(url: string, body: {}, initialiseAppWithUsername?: string): Promise<any> {
 
-    this.logService.trace('POST Request: ' + url + ' with body ', body);
-
     return new Promise((resolve, reject) => {
       this.haventecClient.http.postNoAuth(url, body).then(
         data => {
-
-          this.logService.trace('Response: ' + url + ' with data ', data);
-
           if (initialiseAppWithUsername) {
-            this.logService.debug('Initialising App with username: ' + initialiseAppWithUsername);
             this.haventecClient.init(initialiseAppWithUsername);
           }
 
@@ -46,7 +34,6 @@ export class HttpService {
           resolve(data);
         },
         err => {
-          this.logService.error(err);
           reject(err);
         }
       );
@@ -55,19 +42,13 @@ export class HttpService {
 
   delete(url: string): Promise<any> {
 
-    this.logService.trace('DELETE Request: ' + url);
-
     return new Promise((resolve, reject) => {
       this.haventecClient.http.delete(url, this.haventecClient.getAccessToken()).then(
         data => {
-
-          this.logService.trace('Response: ' + url + ' with data ', data);
-
           this.haventecClient.updateDataFromResponse(data);
           resolve(data);
         },
         err => {
-          this.logService.error(err);
           reject(err);
         }
       );
