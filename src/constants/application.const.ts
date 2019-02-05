2eb05de3e730bd77b9afa508129c646a5f76f9f3
Justin Crosbie <justin.crosbie@haventec.com>
2019-02-05 18:25:50 +1100

    Upgrades packages to fix npm audit security issues

--- src/constants/application.const.ts
+++ src/constants/application.const.ts
@@ -1,8 +1,8 @@
 // Your backend server that communicates with Haventec. Sample backend server available at https://github.com/Haventec/haventec-authenticate-sample-server
-export const API_ENDPOINT: string = '';
+export const API_ENDPOINT: string = 'https://lf7m40x8p9.execute-api.ap-southeast-2.amazonaws.com/prod/haventec-authenticate-sample-server';
 
 // Your Application UUID you received from Haventec Console (cloudportal.haventec.com)
-export const APPLICATION_UUID: string = '';
+export const APPLICATION_UUID: string = '2beff7e8-e57e-4acc-adce-6075098cb4e4';
 
 // Your name of this application that is displayed to the user
 export const APPLICATION_NAME: string = 'Catch Finance';
